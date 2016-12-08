facae28cd9e4236637a894b90f8a3eba46013b32
Eric Lau <ericlau@Corinnas-MacBook-Air.local>
2016-12-07 18:37:57 -0800

    Added basic profile template & fetch method

---
+++ App/Client/Views/profileTemplate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{$route.params.id}}</h1>
+      <div>
+        <div>
+          <div class='profileImg' v-html='profileImg'></div>
+        </div>
+		<div>        
+		  <span>Name :{{name}}</span>
+        </div>
+        <div>
+	      <span>Gender: {{gender}}</span>
+	    </div>
+	    <div>
+	      <span>Age: {{age}}</span>
+	    </div>
+	    <div>
+	      <span>Location: {{location}}</span>
+	    </div>
+		<div>
+	      <span>Description: {{userinfo}}</span>
+	    </div>
+	</div>
+	<!--<div>*Place holder for matchs*</div>-->
+  </div>
+</template>
